5969c3028c3b9ea0067dcce120d64b2dd30a16c6
Jay <jayottovegio@gmail.com>
2013-07-05 11:44:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stanfordclubsf::Application.config.secret_token = 'deafba1f6b8781b19cbeb8f78d37a1f505cc488fa0e0b2ebb02c0e2293e9c53389d81a40acf6dbeced0218a7dfd1d75763ccb15b588ebb7f3516f48615bb3f7d'
