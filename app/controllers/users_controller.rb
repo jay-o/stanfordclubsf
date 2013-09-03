34e501b31c358b6779710083821aa2187344c12a
Jay <jayottovegio@gmail.com>
2013-09-02 22:12:57 -0700

    added styling and more layout tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,34 @@
 class UsersController < ApplicationController
-	load_and_authorize_resource
-	
-	def new
-	end
+  load_and_authorize_resource
+  
+  def new
+  end
 
-	def create
-		if @user.save
-			flash[:notice] = "User Sucessfully Created: #{@user.email}"
-			redirect_to admin_path
-		else
-			render 'new'
-		end
-	end	
+  def create
+    if @user.save
+      flash[:notice] = "User Sucessfully Created: #{@user.email}"
+      redirect_to admin_path
+    else
+      render 'new'
+    end
+  end 
 
-	def show		
-	end
+  def show    
+  end
 
-	def edit	
-	end
+  def edit  
+  end
 
-	def update
-		if @user.update_attributes(params[:user])
+  def update
+    if @user.update_attributes(params[:user])
       flash[:notice] = "Profile updated"
       redirect_to admin_path
     else
       render 'edit'
     end
-	end
+  end
 
-	def index
-		@users = User.all
-	end
+  def index
+    @users = User.all
+  end
 end
