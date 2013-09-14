8b13c5fb6d38b4f919ff8e3edff12b84f53f7b92
Jay <jayottovegio@gmail.com>
2013-09-14 08:15:45 -0700

    fix migration errors

--- db/migrate/20130829232349_add_title_active_to_users.rb
+++ db/migrate/20130829232349_add_title_active_to_users.rb
@@ -2,6 +2,6 @@ class AddTitleActiveToUsers < ActiveRecord::Migration
   def change
     add_column :users, :position, :string
     add_column :users, :committee_id, :integer
-    add_column :users, :state, :string, , :default => "Active"
+    add_column :users, :state, :string, :default => "Active"
   end
 end
