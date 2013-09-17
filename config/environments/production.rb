c11aa77a47e5b8065eb691193d8f250cf2a03a35
Jay <jayottovegio@gmail.com>
2013-09-17 16:32:10 -0700

    updated email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Stanfordclubsf::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => "http://www.stanfordclubsf.org" }  
+  config.action_mailer.default_url_options = { :host => "www.stanfordclubsf.org" }  
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
