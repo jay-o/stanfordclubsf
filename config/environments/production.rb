77949bbac8731ee39e284df11634fd4762dac4a3
Jay <jayottovegio@gmail.com>
2016-04-23 17:58:34 -0700

    updating mailer to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,12 +69,12 @@ Stanfordclubsf::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :port =>           '587',
-      :address =>        'smtp.mandrillapp.com',
-      :user_name =>      ENV['MANDRILL_USERNAME'],
-      :password =>       ENV['MANDRILL_APIKEY'],
-      :domain =>         'heroku.com',
-      :authentication => :plain
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'stanfordclubsf.org',
+      :authentication => :plain,
   }
   
 end
